3ddaa59b02317d9aca39d75d4e6d7070238f5947
Yun-Ting Chang <cha22033@byui.edu>
2022-10-04 13:56:12 -0600

    menu not works

---
+++ chamber/scripts/script.js
@@ -0,0 +1,5 @@
+document.getElementById("updated-time").innerHTML = document.lastModified;
+
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive")
+}
