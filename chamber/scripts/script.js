2bede4db15d307f58a36341cfce4976b09c64feb
Yun-Ting Chang <cha22033@byui.edu>
2022-10-04 16:48:44 -0600

    everything works except menu

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,5 +1,5 @@
 document.getElementById("updated-time").innerHTML = document.lastModified;
 
 function toggleMenu() {
-    document.getElementsByClassName("navigation")[0].classList.toggle("responsive")
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
