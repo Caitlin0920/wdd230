168a0eba09df1d89c2fadb7effa5360762318332
Yun-Ting Chang <cha22033@byui.edu>
2022-10-08 16:38:16 -0600

    date added

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -3,3 +3,14 @@ document.getElementById("updated-time").innerHTML = document.lastModified;
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
+
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date");
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
