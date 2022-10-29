a3465ae0d0df3037e7028880ced1bb41cad123ab
Yun-Ting Chang <cha22033@byui.edu>
2022-10-29 13:23:34 -0600

    lesson 7 finished

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -17,6 +17,31 @@ const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 datefield.innerHTML = `<em>${fulldate}</em>`;
 
 
-if (now.getDay() == 1 || now.getDay() == 2) {
+if (now.getDay() === 1 || now.getDay() === 2) {
     document.getElementById("banner").textContent = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m."
 }
+
+// initialize display elements
+const visitsDisplay = document.querySelector("#last-visited");
+
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+let lastVisitDate = new Date(window.localStorage.getItem("last-visit-time"));
+
+// determine if this is the first visit or display the number of visits.
+if (numVisits > 1) {
+    const currentVisitDate = new Date();
+    const msDifference = currentVisitDate.getTime() - lastVisitDate.getTime();
+    const dayDifference = Math.round(msDifference / (1000 * 3600 * 24));
+    visitsDisplay.textContent = dayDifference + " days ago";
+    lastVisitDate = currentVisitDate
+} else {
+    visitsDisplay.textContent = `This is your first visit!`;
+    lastVisitDate = new Date();
+}
+
+// increment the number of visits.
+numVisits++;
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits.toString());
+localStorage.setItem("last-visit-time", lastVisitDate);
