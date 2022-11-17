49a97241ba9c57a103274786a97df951d9ef56fb
Yun-Ting Chang <cha22033@byui.edu>
2022-11-17 15:44:52 -0700

    api applied

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,19 +1,47 @@
-let temperature = document.getElementById("temperature");
+let weatherIcon = document.querySelector("#weather-icon");
+let temperature = document.querySelector("#temperature")
 let speed = document.getElementById("speed");
-let t = parseFloat(temperature.innerText);
-let s = parseFloat(speed.innerText);
 let chill = document.getElementById("chill");
+let description  = document.getElementById("description")
+let url = "https://api.openweathermap.org/data/2.5/weather?q=Provo&appid=3a614d14e0c682019daff2aad743ba7a&units=imperial"
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        }else {
+            Error(await response.text());
+        }
+    }catch (error) {
+        console.log(error);
+    }
+}
+apiFetch()
+
+function displayResults(weatherData) {
+    let tInF = parseFloat(weatherData.main.temp);
+    temperature.innerHTML = tInF.toFixed(0);
+    let sInMph = parseFloat(weatherData.wind.speed);
+    speed.textContent = sInMph;
+
+    let iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    let desc = weatherData.weather[0].description;
+
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    description.textContent = desc;
+
+    if (tInF < 50 && sInMph > 3) {
+        chill.textContent = String("feels like " + calculateWindChill(tInF, sInMph) + "°F");
+    }else {
+        chill.textContent = "N/A";
+    }
+}
 
 function calculateWindChill(temperature, speed) {
-    let tInF = (temperature * 1.8) + 32;
-    let sInMph = speed * 0.621371192;
-    let windChillInF = 35.74 + (0.6215 * tInF) - (35.75 * sInMph ** 0.16) + (0.4275 * tInF * sInMph ** 0.16);
-    let windChillInC = (windChillInF - 32) / 1.8;
-    return windChillInC.toFixed(2);
-};
-
-if (t < 10 && s > 4.8) {
-    chill.textContent = String("feels like " + calculateWindChill(t, s) + "°C");
-}else {
-    chill.textContent = "N/A";
+    let windChillInF = 35.74 + (0.6215 * temperature) - (35.75 * speed ** 0.16) + (0.4275 * temperature * speed ** 0.16);
+    return windChillInF.toFixed(2);
 }
